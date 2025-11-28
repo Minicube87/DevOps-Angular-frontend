59e798856479396a71f8cc3fbc3e918b70dc9485
Minicube87 <Jan.Sulga@skyered-education.de>
2025-11-28 09:56:20 +0100

    new app.component.spec.ts

--- angular-frontend/src/app/app.component.spec.ts
+++ angular-frontend/src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-frontend'`, () => {
+  it(`should have as title 'Angular 10 Spring Boot CRUD Full Stack App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-frontend');
+    expect(app.title).toEqual('Angular 10 Spring Boot CRUD Full Stack App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'angular-frontend app is running!'
+    );
   });
 });
